8df826fcde5ea4e3aa32520c7ef62c32afa78a49
rafaelBporto <rafaelbporto88@gmail.com>
2023-01-05 17:32:44 -0300

    feat: post /publish-post

--- src/Pages/Timeline/components/PublishPost/PublishPostForm.js
+++ src/Pages/Timeline/components/PublishPost/PublishPostForm.js
@@ -1,14 +1,56 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { StylePublishPostForm } from "../../../../assets/css/PublishPostStyles/StylePublishPostForm";
+import BASE_URL from "../../../../constants/URL";
 
-export default function PublishPostForm(){
+export default function PublishPostForm() {
+  const [link, setLink] = useState("");
+  const [description, setDescription] = useState("")
 
-    return(
-     <StylePublishPostForm>
-        <h1> What are you going to share today?</h1>
-       <input placeholder="http://..."/>
-       <input type="text" placeholder="Awesome article about #javascript"/>
-       <button>Publish</button>
-     </StylePublishPostForm>
-    )
+  const token = localStorage.getItem("token")
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+
+    const body = {
+      link,
+      description
+    }
+
+    try {
+      await axios.post(`${BASE_URL}/publish-post`, body,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        });
+      setLink("");
+      setDescription("");
+    } catch (error) {
+      console.log(error);
+    }
+  }
+  return (
+    <StylePublishPostForm onSubmit={handleSubmit}>
+      <h1> What are you going to share today?</h1>
+      <input
+        name="link"
+        value={link}
+        type="url"
+        placeholder="https://..."
+        onChange={(e) => setLink(e.target.value)}
+        required
+      />
+
+      <input
+        name="description"
+        value={description}
+        type="text"
+        placeholder="Awesome article about #javascript"
+        onChange={(e) => setDescription(e.target.value)}
+        required
+      />
+      <button type="onSubmit">Publish</button>
+    </StylePublishPostForm>
+  )
 }
