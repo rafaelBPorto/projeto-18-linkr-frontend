bb2411c604e36a0dd11f5d8108a6f55eae1358e9
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2023-01-05 20:45:30 -0300

    fix: removed header from signUp page

--- src/Pages/SignUp/SignUp.js
+++ src/Pages/SignUp/SignUp.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Header from "../Contants/Header.js";
 import { Banner } from "../SignIn/Components/signInComponents.js";
 import { Container } from "../SignIn/SignIn.js";
 import { SignUpForm } from "./signUpComponents.js";
@@ -9,7 +8,6 @@ export default function SignUp(){
 
     return(
         <Container>
-            <Header></Header>
             <Banner></Banner>
             <SignUpForm></SignUpForm>
         </Container>
