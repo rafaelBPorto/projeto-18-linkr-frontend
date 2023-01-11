ef94a656362e7bf172421aa4515747684210af1a
EnzoMacedo11 <enzomacedo15@gmail.com>
2023-01-11 14:20:46 -0300

    feat: alteração do header

--- src/Pages/Timeline/Timeline.js
+++ src/Pages/Timeline/Timeline.js
@@ -36,7 +36,7 @@ export default function TimeLine() {
 
     return (
         <>
-            {/* <Header /> */}
+            <Header /> 
             <StylePage>
                 <StyleDescriptionPAge>
                     timeline
