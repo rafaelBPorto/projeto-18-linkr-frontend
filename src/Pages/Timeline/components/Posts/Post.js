b94beb53c20a8f31409be9ed4d21ece9565135b1
EmanuelMarcolongo <emanuelmarcolongo@gmail.com>
2023-01-12 14:26:33 -0300

    feat: tagify add to timeline

--- src/Pages/Timeline/components/Posts/Post.js
+++ src/Pages/Timeline/components/Posts/Post.js
@@ -1,77 +1,100 @@
 import React from "react";
 import { StylePost } from "../../../../assets/css/PostStyles.js/StylePost";
 import { StylePostDescription } from "../../../../assets/css/PostStyles.js/StylePostDescription";
-import { StylePostIcon, StylePostLeft } from "../../../../assets/css/PostStyles.js/StylePostLeft";
-import { StylePostLink, StylePostLinkImg, StylePostLinkText } from "../../../../assets/css/PostStyles.js/StylePostLink";
+import {
+  StylePostIcon,
+  StylePostLeft,
+} from "../../../../assets/css/PostStyles.js/StylePostLeft";
+import {
+  StylePostLink,
+  StylePostLinkImg,
+  StylePostLinkText,
+} from "../../../../assets/css/PostStyles.js/StylePostLink";
 import { StyleUserImg } from "../../../../assets/css/StyleUserImg";
-import trahsIcon from "../../../../assets/imgs/trahsIcon.svg"
-import heartOutline from "../../../../assets/imgs/heartOutline.svg"
+import trahsIcon from "../../../../assets/imgs/trahsIcon.svg";
+import heartOutline from "../../../../assets/imgs/heartOutline.svg";
 import axios from "axios";
 import BASE_URL from "../../../../constants/URL";
+import { AiOutlineHeart, AiFillHeart } from "react-icons/ai";
+import { useNavigate } from "react-router-dom";
+import { ReactTagify } from "react-tagify";
+import { tagStyle } from "../../../../Components/styleTagify";
 
+export default function Post({ post, setUpdate, token, user }) {
+  const {
+    id,
+    user_id,
+    description,
+    user_name,
+    user_photo,
+    link_title,
+    link_descripition,
+    link_url,
+    link_image,
+  } = post;
+  const userId = user.id;
 
-export default function Post( {post, setUpdate, token, user} ) {
+  const navigate = useNavigate();
+ 
+  const authorization = {
+    headers: {
+      authorization: `Bearer ${token}`,
+    },
+  };
 
-    const { id, user_id, description, user_name,user_photo , link_title, link_descripition, link_url, link_image} = post;
-    const userId = user.id;
-
-
-    const authorization = {
-        headers: {
-            authorization: `Bearer ${token}`,
-        }
-    };
-
-
-    async function deletePost(postId) {
-      
-        try {
-            await axios.delete(`${BASE_URL}/delete-post/${postId}`,
-                authorization
-            );
-            
-        } catch (error) {
-            alert("Não foi possível excluir o post!")
-        }
-        
-        setUpdate(true)
-
+  async function deletePost(postId) {
+    try {
+      await axios.delete(`${BASE_URL}/delete-post/${postId}`, authorization);
+    } catch (error) {
+      alert("Não foi possível excluir o post!");
     }
 
-    return (
-        <StylePost>
+    setUpdate(true);
+  }
 
-            <StylePostLeft>
-                <StyleUserImg src={user_photo} />
+  return (
+    <StylePost>
+      <StylePostLeft>
+        <StyleUserImg src={user_photo} />
 
-                <StylePostIcon src={heartOutline} />
-            </StylePostLeft>
+        <StylePostIcon src={heartOutline} />
+      </StylePostLeft>
 
-            <div>
-                <StylePostDescription>
-                    <h1>
+      <div>
+        <StylePostDescription>
+          <h1>
+            {user_name}
+            {userId === user_id && (
+              <img src={trahsIcon} alt="trash" onClick={() => deletePost(id)} />
+            )}
+          </h1>
 
-                        {user_name}
-                        {userId=== user_id && (
-                            <img src={trahsIcon} alt="trash" onClick={() => deletePost(id)} />
-                        )}
 
-                      
+          <ReactTagify
+            tagStyle={tagStyle}
+            tagClicked={(tag) => {
+              const newTag = tag.replace("#", "");
+              navigate(`/hashtag/${newTag}`);
+            }}
+          >
+            <p>{description}</p>
+          </ReactTagify>
 
-                    </h1>
-                    <p>{description}</p>
-                </StylePostDescription>
-                <StylePostLink href={link_url} target="_blank" rel="noopener noreferrer">
-                    <StylePostLinkText>
-                        <h1>{link_title}</h1>
-                        <h2>{link_descripition}</h2>
-                        <p>{link_url}</p>
-                    </StylePostLinkText>
-                    <StylePostLinkImg src={link_image} />
-
-                </StylePostLink>
-            </div>
-
-        </StylePost>
-    )
+          
+        </StylePostDescription>
+        <StylePostLink
+          href={link_url}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <StylePostLinkText>
+            <h1>{link_title}</h1>
+            <h2>{link_descripition}</h2>
+            <p>{link_url}</p>
+          </StylePostLinkText>
+          <StylePostLinkImg src={link_image} />
+        </StylePostLink>
+      </div>
+    </StylePost>
+  );
 }
